27f8d29ee4be51f8cca16de48cc0ef90834dc92b
meeae <meeaeking@gmail.com>
2019-07-03 18:43:00 -0500

    make migration files to change description data type to text and change price to decimal

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -15,7 +15,8 @@ class Api::ProductsController < ApplicationController
       name: params[:name],
       description: params[:description],
       price: params[:price],
-      image_url: params[:image_url]
+      image_url: params[:image_url],
+      inventory: params[:inventory]
     )
     @product.save
     render 'show.json.jb'
@@ -24,9 +25,10 @@ class Api::ProductsController < ApplicationController
   def update
     the_id = params["id"]
     @product = Product.find_by(id: the_id)
-    @product.name = params[:update_name]
-    @product.description = params[:update_description]
-    @product.price = params[:update_price]
+    @product.name = params[:name]
+    @product.description = params[:description]
+    @product.price = params[:price]
+    @product.inventory = params[:inventory]
 
     render 'show.json.jb'
   end
---
+++ db/migrate/20190703232347_make_changes.rb
@@ -0,0 +1,6 @@
+class MakeChanges < ActiveRecord::Migration[6.0]
+  def change
+    change_column :products, :description, :text
+    change_column :products, :price, :decimal, precision: 5, scale: 2
+  end
+end
