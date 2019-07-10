f9a2dfd3146170caf2517a7acf07b35c2fe24ae7
meeae <meeaeking@gmail.com>
2019-07-09 19:37:57 -0500

    add image table and connect by using has many and belongs to

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,9 +7,14 @@ class Product < ApplicationRecord
   validates :description, length: {in: 10..500}
   validates :description, presence: true
 
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
+  belongs_to :supplier
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
+  has_many :images
+  # def images
+  #   Image.where(product_id: id)
+  # end
 
   def is_discounted?
     if price.to_i < 10
